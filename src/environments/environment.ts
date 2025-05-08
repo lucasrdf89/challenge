377612c6c5568d7fb89e18ad9800c1f57f683ce2
Lucas Rafael de la Fuente <lucasrafaeldelafuente@MacBook-Air-de-Lucas.local>
2025-05-08 15:51:33 -0300

    workflows

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     NG_APP_JWT_SECRET: "3UAXXroYIi",
     API_URL: "https://api-xvqtfpnzhq-uc.a.run.app/",
+    FIREBASE_TOKEN: "AIzaSyD-3vX2r0x1g4bq5c6j7f8k9l0m1n2o3p4",
 };
