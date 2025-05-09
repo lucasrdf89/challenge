b6e7631e7bcdcf9c104af8b3a25005fde96e6db0
Lucas Rafael de la Fuente <lucasrafaeldelafuente@MacBook-Air-de-Lucas.local>
2025-05-08 23:46:59 -0300

    fixes

--- src/app/modules/tasks/tasks.component.ts
+++ src/app/modules/tasks/tasks.component.ts
@@ -57,12 +57,12 @@ export class TasksComponent implements OnInit, OnDestroy {
 
     async updateTask(updatedTask: Task): Promise<void> {
         const resp: boolean = await this.taskService.updateTask(updatedTask);
-        if(resp) {
+        if (resp) {
             this.tasks = this.tasks.map((task: Task) => task.id === updatedTask.id ? updatedTask : task);
             this.notification.success("Task updated successfully");
         } else {
-            this.notification.error("Error updating task");            
-        }        
+            this.notification.error("Error updating task");
+        }
     }
 
     async deleteTask(task: Task): Promise<void> {
